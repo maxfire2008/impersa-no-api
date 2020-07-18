76ec867347c391f0aa4b93bc6ee067e04bb216ff
maxrichardburgess <maxrichardburgess@users.noreply.github.com>
2020-07-18 16:18:59 +1000

    added first bit of api and index page

---
+++ impersa_no/index.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def initiate_verification(request):
+    return HttpResponse("This is an api. Go to impersa-no.maxstuff.net")
---
+++ impersa_no/initiate_verification.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+import json
+
+def initiate_verification(request):
+    verifying_employee_webhook = request.GET.get("verify_employee_webhook",None)
+    employee_id = request.GET.get("employee_id",None)
+    company_id = request.GET.get("company_id",None)
+    if verifying_employee_webhook and employee_id and company_id:
+        response = json.dumps({
+            "customer":customer_link,
+            "employee":employee_link
+            })
+        return HttpResponse(response)
+    else:
+        return HttpResponse("error",status=400)
